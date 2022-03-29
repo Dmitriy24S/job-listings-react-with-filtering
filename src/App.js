12f570dbeb5e0eac19e6c398ed2d4ad9896e66fe
Dmitriy <devilins24@gmail.com>
2022-03-29 22:09:18 +0300

    add featured card style

--- src/App.js
+++ src/App.js
@@ -55,83 +55,80 @@ function App() {
         )}
         {filteredItems.map((item, index) => {
           return (
-            <div className="card-wrapper" key={index}>
-              <div className="left-accent"></div>
-              <div className="card" key={item.id}>
-                {/* Logo */}
-                <div className="company-logo">
-                  <img src={item.logo} alt="company" />
-                </div>
-                <div className="info-container">
-                  {/* main info */}
-                  <div className="company-header">
-                    <h3 className="company-name">{item.company}</h3>
-                    <div className="notification-container">
-                      {item.new && <span className="notification new-noti">NEW!</span>}
-                      {item.featured && <span className="notification feature-noti">FEATURED</span>}
-                    </div>
-                  </div>
-                  <a href="/#" className="job-title">
-                    {item.position}
-                  </a>
-                  {/* sub info */}
-                  <div className="post-info">
-                    <ul>
-                      <li>{item.postedAt}</li>
-                      <li>{item.contract} </li>
-                      <li>{item.location}</li>
-                    </ul>
+            <div className={`card ${item.featured ? "featured-card" : ""}`} key={item.id}>
+              {/* Logo */}
+              <div className="company-logo">
+                <img src={item.logo} alt="company" />
+              </div>
+              <div className="info-container">
+                {/* main info */}
+                <div className="company-header">
+                  <h3 className="company-name">{item.company}</h3>
+                  <div className="notification-container">
+                    {item.new && <span className="notification new-noti">NEW!</span>}
+                    {item.featured && <span className="notification feature-noti">FEATURED</span>}
                   </div>
                 </div>
-                {/* Tags */}
-                <div className="tags">
-                  {/* role tag */}
-                  <button
-                    onClick={(e) => {
-                      selectTag(e);
-                    }}
-                    className="tag-btn"
-                  >
-                    {item.role}
-                  </button>
-                  {/* level tag */}
-                  <button
-                    onClick={(e) => {
-                      selectTag(e);
-                    }}
-                    className="tag-btn"
-                  >
-                    {item.level}
-                  </button>
-                  {/* languges tags */}
-                  {item.languages.map((item, index) => {
-                    return (
-                      <button
-                        className="tag-btn"
-                        onClick={(e) => {
-                          selectTag(e);
-                        }}
-                        key={index}
-                      >
-                        {item}
-                      </button>
-                    );
-                  })}
-                  {/* tools tags */}
-                  {item.tools.map((item, index) => {
-                    return (
-                      <button
-                        className="tag-btn"
-                        key={index}
-                        onClick={(e) => {
-                          selectTag(e);
-                        }}
-                      >
-                        {item}
-                      </button>
-                    );
-                  })}
+                <a href="/#" className="job-title">
+                  {item.position}
+                </a>
+                {/* sub info */}
+                <div className="post-info">
+                  <ul>
+                    <li>{item.postedAt}</li>
+                    <li>{item.contract} </li>
+                    <li>{item.location}</li>
+                  </ul>
                 </div>
+              </div>
+              {/* Tags */}
+              <div className="tags">
+                {/* role tag */}
+                <button
+                  onClick={(e) => {
+                    selectTag(e);
+                  }}
+                  className="tag-btn"
+                >
+                  {item.role}
+                </button>
+                {/* level tag */}
+                <button
+                  onClick={(e) => {
+                    selectTag(e);
+                  }}
+                  className="tag-btn"
+                >
+                  {item.level}
+                </button>
+                {/* languges tags */}
+                {item.languages.map((item, index) => {
+                  return (
+                    <button
+                      className="tag-btn"
+                      onClick={(e) => {
+                        selectTag(e);
+                      }}
+                      key={index}
+                    >
+                      {item}
+                    </button>
+                  );
+                })}
+                {/* tools tags */}
+                {item.tools.map((item, index) => {
+                  return (
+                    <button
+                      className="tag-btn"
+                      key={index}
+                      onClick={(e) => {
+                        selectTag(e);
+                      }}
+                    >
+                      {item}
+                    </button>
+                  );
+                })}
               </div>
             </div>
           );
